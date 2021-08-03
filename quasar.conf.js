b400716eb5c53e38f6e7741657c75e5fc7f5ff9c
Faruk D <f.diblen@esciencecenter.nl>
2021-08-03 21:02:37 +0200

    use cffinit pulicpath for io page

--- quasar.conf.js
+++ quasar.conf.js
@@ -52,8 +52,8 @@ module.exports = configure(function (ctx) {
 
     // Full list of options: https://v2.quasar.dev/quasar-cli/quasar-conf-js#Property%3A-build
     build: {
-      vueRouterMode: 'hash', // available values: 'hash', 'history'
-      // publicPath: '.',
+      vueRouterMode: 'history', // available values: 'hash', 'history'
+      publicPath: 'cffinit',
       // transpile: false,
 
       // Add dependencies for transpiling with Babel (Array of string/regex)

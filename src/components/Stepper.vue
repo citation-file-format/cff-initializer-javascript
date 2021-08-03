49cf6d9a5e73de344b08ed3d334a8a1af5d71fc0
Faruk D <f.diblen@esciencecenter.nl>
2021-08-03 17:15:20 +0200

    add stepper, create a 3-colums layout

--- src/components/Stepper.vue
+++ src/components/Stepper.vue
@@ -1,49 +1,76 @@
 <template>
-  <q-item
-    clickable
-    :to="link"
-  >
-    <q-item-section
-      v-if="icon"
-      avatar
+  <div class="q-pa-md">
+    <q-stepper
+      v-model="step"
+      vertical
+      color="primary"
+      animated
     >
-      <q-icon :name="icon" />
-    </q-item-section>
+      <q-step
+        :name="1"
+        title="Start"
+        icon="star"
+        :done="step > 1"
+      >
+        For each ad campaign that you create, you can control how much you're willing to
+        spend on clicks and conversions, which networks and geographical locations you want
+        your ads to show on, and more.
 
-    <q-item-section>
-      <q-item-label>{{ title }}</q-item-label>
-      <q-item-label caption>
-        {{ caption }}
-      </q-item-label>
-    </q-item-section>
-  </q-item>
+        <q-stepper-navigation>
+          <q-btn @click="step = 2" color="primary" label="Continue" />
+        </q-stepper-navigation>
+      </q-step>
+
+      <q-step
+        :name="2"
+        title="Create an ad group"
+        caption="Optional"
+        icon="create_new_folder"
+        :done="step > 2"
+      >
+        An ad group contains one or more ads which target a shared set of keywords.
+
+        <q-stepper-navigation>
+          <q-btn @click="step = 4" color="primary" label="Continue" />
+          <q-btn flat @click="step = 1" color="primary" label="Back" class="q-ml-sm" />
+        </q-stepper-navigation>
+      </q-step>
+
+      <q-step
+        :name="3"
+        title="Ad template"
+        icon="assignment"
+        disable
+      >
+        This step won't show up because it is disabled.
+      </q-step>
+
+      <q-step
+        :name="4"
+        title="Create an ad"
+        icon="add_comment"
+      >
+        Try out different ad text to see what brings in the most customers, and learn how to
+        enhance your ads using features like ad extensions. If you run into any problems with
+        your ads, find out how to tell if they're running and how to resolve approval issues.
+
+        <q-stepper-navigation>
+          <q-btn color="primary" label="Finish" />
+          <q-btn flat @click="step = 2" color="primary" label="Back" class="q-ml-sm" />
+        </q-stepper-navigation>
+      </q-step>
+    </q-stepper>
+  </div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue'
+<script>
+import { ref } from 'vue'
 
-export default defineComponent({
-  name: 'Stepper',
-  props: {
-    title: {
-      type: String,
-      required: true
-    },
-
-    caption: {
-      type: String,
-      default: ''
-    },
-
-    link: {
-      type: String,
-      default: '#'
-    },
-
-    icon: {
-      type: String,
-      default: ''
+export default {
+  setup () {
+    return {
+      step: ref(1)
     }
   }
-})
+}
 </script>
--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,6 +1,6 @@
 <template>
-  <q-layout view="lHh Lpr lFf">
-    <q-header elevated>
+  <q-layout view="hHh lpR fff">
+    <q-header reveal bordered elevated>
       <q-toolbar>
         <q-btn
           flat
@@ -19,35 +19,29 @@
       </q-toolbar>
     </q-header>
 
-    <q-drawer
-      v-model="leftDrawerOpen"
-      show-if-above
-      bordered
-      class="bg-grey-1"
-    >
-      <q-list>
-        <q-item-label
-          header
-          class="text-grey-8"
-        >
-          Essential Links
-        </q-item-label>
-
-        <Stepper
+    <div class="row">
+      <div class="col-2">
+        <!-- <SideMenu
           v-for="link in essentialLinks"
           :key="link.title"
           v-bind="link"
-        />
-      </q-list>
-    </q-drawer>
-
-    <q-page-container>
-      <router-view />
-    </q-page-container>
+        /> -->
+        <Stepper />
+      </div>
+      <div class="col-6">
+        <q-page-container>
+          <router-view />
+        </q-page-container>
+      </div>
+      <div class="col-4">
+        CFF preview
+      </div>
+    </div>
   </q-layout>
 </template>
 
 <script lang="ts">
+import SideMenu from 'src/components/SideMenu.vue'
 import Stepper from 'src/components/Stepper.vue'
 
 const linksList = [
@@ -77,6 +71,7 @@ export default defineComponent({
   name: 'MainLayout',
 
   components: {
+    SideMenu,
     Stepper
   },
 
@@ -93,3 +88,24 @@ export default defineComponent({
   }
 })
 </script>
+
+<style scoped>
+
+.row {
+  margin-left: 200px;
+  margin-right: 200px;
+}
+
+.col-2 {
+  border-style: solid;
+}
+
+.col-6 {
+  border-style: solid;
+}
+
+.col-4 {
+  border-style: solid;
+}
+
+</style>

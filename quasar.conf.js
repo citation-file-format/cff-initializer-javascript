d4ea0a3b1d6554fec6a766372c20af422ed1031f
Faruk D <f.diblen@esciencecenter.nl>
2021-08-03 20:57:18 +0200

    add public path to quasar config

--- quasar.conf.js
+++ quasar.conf.js
@@ -53,7 +53,7 @@ module.exports = configure(function (ctx) {
     // Full list of options: https://v2.quasar.dev/quasar-cli/quasar-conf-js#Property%3A-build
     build: {
       vueRouterMode: 'history', // available values: 'hash', 'history'
-
+      publicPath: '.',
       // transpile: false,
 
       // Add dependencies for transpiling with Babel (Array of string/regex)

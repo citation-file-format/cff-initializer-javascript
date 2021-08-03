c98f1d220027a0ed246144caaa91d1870576bee0
Faruk D <f.diblen@esciencecenter.nl>
2021-08-03 16:41:03 +0200

    improve the side menu

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -52,46 +52,22 @@ import Stepper from 'src/components/Stepper.vue'
 
 const linksList = [
   {
-    title: 'Docs',
-    caption: 'quasar.dev',
+    title: 'Start',
+    caption: '',
     icon: 'school',
-    link: 'https://quasar.dev'
+    link: '/start'
   },
   {
-    title: 'Github',
-    caption: 'github.com/quasarframework',
-    icon: 'code',
-    link: 'https://github.com/quasarframework'
+    title: 'Authors',
+    caption: '',
+    icon: 'account-group',
+    link: '/authors'
   },
   {
-    title: 'Discord Chat Channel',
-    caption: 'chat.quasar.dev',
-    icon: 'chat',
-    link: 'https://chat.quasar.dev'
-  },
-  {
-    title: 'Forum',
-    caption: 'forum.quasar.dev',
-    icon: 'record_voice_over',
-    link: 'https://forum.quasar.dev'
-  },
-  {
-    title: 'Twitter',
-    caption: '@quasarframework',
-    icon: 'rss_feed',
-    link: 'https://twitter.quasar.dev'
-  },
-  {
-    title: 'Facebook',
-    caption: '@QuasarFramework',
-    icon: 'public',
-    link: 'https://facebook.quasar.dev'
-  },
-  {
-    title: 'Quasar Awesome',
-    caption: 'Community Quasar projects',
-    icon: 'favorite',
-    link: 'https://awesome.quasar.dev'
+    title: 'Finish',
+    caption: '',
+    icon: 'certificate',
+    link: '/finish'
   }
 ]
 

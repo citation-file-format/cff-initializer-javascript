1d21dd2569823255e7351ed35347173f0f3c9279
Faruk D <f.diblen@esciencecenter.nl>
2021-08-03 15:48:38 +0200

    use ionicons

--- quasar.conf.js
+++ quasar.conf.js
@@ -38,7 +38,7 @@ module.exports = configure(function (ctx) {
 
     // https://github.com/quasarframework/quasar/tree/dev/extras
     extras: [
-      // 'ionicons-v4',
+      'ionicons-v4',
       // 'mdi-v5',
       // 'fontawesome-v5',
       // 'eva-icons',

b589953392752e0de4eac5bf7204b20f0addfe04
Faruk D <f.diblen@esciencecenter.nl>
2021-08-04 16:38:07 +0200

    improve layout of start page

--- src/pages/Start.vue
+++ src/pages/Start.vue
@@ -1,11 +1,17 @@
 <template>
-    <q-page-container>
-      <q-page>
-        <h1>Landing Page</h1>
-        <q-btn color="grey-6" label="Continue" to="/1"/>
-      </q-page>
-    </q-page-container>
-
+  <div class="q-pa-md">
+    <q-layout view="lHh lpr lFf" class="shadow-2 rounded-borders">
+      <q-page-container>
+        <q-page padding>
+          <h1>Start Page</h1>
+          <q-btn color="grey-6" label="Continue" to="/1"/>
+          <p v-for="n in 10" :key="n">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit nihil praesentium molestias a adipisci, dolore vitae odit, quidem consequatur optio voluptates asperiores pariatur eos numquam rerum delectus commodi perferendis voluptate?
+          </p>
+        </q-page>
+      </q-page-container>
+    </q-layout>
+  </div>
 </template>
 
 <script lang="ts">

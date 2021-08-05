7539fdc7f457a4c73b607820b6d4b3ceadd9b88f
Faruk D <f.diblen@esciencecenter.nl>
2021-08-05 14:06:49 +0200

    have separate stores for step and cff

---
+++ src/store/cff.ts
@@ -0,0 +1,17 @@
+import { ref, computed } from 'vue'
+
+interface CFF {
+  title?: string
+}
+
+const cff = ref<CFF>({
+})
+
+export function useCFF () {
+  return {
+    cff: computed(() => cff.value),
+    title: computed(() => cff.value.title),
+    setTitle: (newTitle: string) => { cff.value.title = newTitle },
+    asYAML: computed(() => JSON.stringify(cff.value, null))
+  }
+}

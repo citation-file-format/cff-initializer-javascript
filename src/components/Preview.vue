28ed0dc8f65e192201ad24bd3f026356296dd860
Faruk D <f.diblen@esciencecenter.nl>
2021-08-05 11:53:15 +0200

    add simple preview for the generated cff

--- src/components/Preview.vue
+++ src/components/Preview.vue
@@ -1,11 +1,21 @@
 <template>
-  <h1>CFF preview</h1>
+  <q-card>
+    <q-card-section>
+      <pre>{{ JSON.stringify(store.state.cff, null, '\t') }}</pre>
+    </q-card-section>
+  </q-card>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { inject, defineComponent } from 'vue'
 
 export default defineComponent({
-  name: 'Preview'
+  name: 'Preview',
+  setup () {
+    const store = inject('store')
+    return {
+      store
+    }
+  }
 })
 </script>

7233aaff9d61a65281eea12c4073fc0cf4e673e5
Faruk D <f.diblen@esciencecenter.nl>
2021-08-05 11:57:18 +0200

    use new store in stepper

--- src/components/Stepper.vue
+++ src/components/Stepper.vue
@@ -1,9 +1,12 @@
 <template>
   <div class="q-pa-md">
 
+      <!-- :modelValue="step.step"
+      @update:modelValue="step.goto" -->
+
     <q-stepper
-      :modelValue="step.step"
-      @update:modelValue="step.goto"
+      v-model="store.state.progress"
+      ref="stepper"
       vertical
       animated
       header-nav
@@ -14,25 +17,18 @@
       <q-step
         title="Start"
         icon="star"
-        :name="0"
-        :done="step.step > 0"
-      >
-        <q-stepper-navigation>
-          <q-btn @click="step.next()" color="grey-6" label="Continue" to="/screen2"/>
-        </q-stepper-navigation>
-      </q-step>
+        :name="1"
+        :done="store.state.progress > 1"
+        v-on:click="showStep"
+      />
 
       <q-step
         title="Authors"
         icon="done_all"
-        :name="1"
-        :done="step.step > 1"
-      >
-        <q-stepper-navigation>
-          <q-btn @click="step = 2" color="grey-6" label="Continue" to="/finish" />
-          <q-btn flat @click="step = 1" color="grey-6" label="Back" class="q-ml-sm" />
-        </q-stepper-navigation>
-      </q-step>
+        :name="2"
+        :done="store.state.progress > 2"
+        v-on:click="showStep"
+      />
 
       <q-step
         :name="100"
@@ -43,22 +39,26 @@
         This step won't show up because it is disabled.
       </q-step>
     </q-stepper>
-
-    Step: {{ step.step.value }}
-
+    Store-progress: {{ store.state.progress }}
   </div>
 </template>
 
 <script>
 
-import { useStep } from '../store'
+import { inject } from 'vue'
+import { useRouter } from 'vue-router'
 
 export default {
   setup () {
-    const step = useStep()
+    const store = inject('store')
+
+    const showStep = () => {
+      console.log(store.state.progress)
+    }
 
     return {
-      step
+      store,
+      showStep
     }
   }
 }

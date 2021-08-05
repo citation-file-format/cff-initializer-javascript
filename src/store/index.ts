454612e4daf2d0da0ceb6d5d68569c0578ccf3ee
Faruk D <f.diblen@esciencecenter.nl>
2021-08-05 11:47:23 +0200

    add new store

---
+++ src/store/index.ts
@@ -0,0 +1,35 @@
+import { reactive } from 'vue'
+
+const state = reactive({
+  progress: 1,
+  progressMax: 2,
+  cff: {
+    title: ''
+  }
+})
+
+const methods = {
+  nextStep () {
+    if (state.progress < state.progressMax) {
+      state.progress++
+    }
+    return state.progress
+  },
+  previousStep () {
+    if (state.progress > 2) {
+      state.progress--
+    }
+    return state.progress
+  }
+}
+
+const getters = {
+
+}
+
+export default {
+  // state: readonly(state),
+  state,
+  methods,
+  getters
+}

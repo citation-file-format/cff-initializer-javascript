ed57d409abfdb7d4265946f0c4eb6d57d00dc5c4
Faruk D <f.diblen@esciencecenter.nl>
2021-08-05 11:50:13 +0200

    show state of the progress in header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,12 +1,13 @@
 <template>
-  <q-header class="bg-grey-4 text-white">
+  <q-header class="bg-grey-4 text-black">
     <q-toolbar>
+      Store-progress: {{ store.state.progress }}
       <q-space ></q-space>
       <q-toolbar-title align="right">
           <q-btn-group flat>
             <q-btn flat color="" label="Report an issue" icon-right="ion-logo-github" type="a" href="" target="_blank" />
-            <q-btn flat color="" label="Documentation" icon-right="" />
-            <q-btn flat color="" label="About" icon-right="" />
+            <q-btn flat color="" label="Documentation" />
+            <q-btn flat color="" label="About" />
           </q-btn-group>
       </q-toolbar-title>
     </q-toolbar>
@@ -14,10 +15,19 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { inject, defineComponent } from 'vue'
 
 export default defineComponent({
-  name: 'Header'
+  name: 'Header',
+
+  setup () {
+    const store = inject('store')
+
+    return {
+      store
+    }
+  }
+
 })
 </script>
 

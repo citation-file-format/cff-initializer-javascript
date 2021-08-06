4dec6fad0ddd52775c28a9a97bad418d4b910fe7
Faruk D <f.diblen@esciencecenter.nl>
2021-08-06 09:26:29 +0200

    fix linting issues

--- src/pages/Finish.vue
+++ src/pages/Finish.vue
@@ -1,30 +1,33 @@
 <template>
-    <div class="window-height window-width row justify-center items-center">
+  <div class="window-height window-width row justify-center items-center">
     <q-card>
       <q-card-title><h1>Finish Page</h1></q-card-title>
       <q-card-main>
-          <p v-for="n in 10" :key="n">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit nihil praesentium molestias a adipisci, dolore vitae odit, quidem consequatur optio voluptates asperiores pariatur eos numquam rerum delectus commodi perferendis voluptate?
-          </p>
+        <p
+          v-for="n in 10"
+          :key="n"
+        >
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit nihil praesentium molestias a adipisci, dolore vitae odit, quidem consequatur optio voluptates asperiores pariatur eos numquam rerum delectus commodi perferendis voluptate?
+        </p>
       </q-card-main>
 
-      <q-card-separator/>
+      <q-card-separator />
 
       <q-card-actions align="center">
-                  <q-btn-group flat>
-            <q-btn
-              v-on:click="step = 1"
-              color="grey-6"
-              label="Create another"
-              to="/"
-            />
-            <q-btn
-              v-on:click="step = 1"
-              color="grey-6"
-              label="Back to form"
-              to="/"
-            />
-          </q-btn-group>
+        <q-btn-group flat>
+          <q-btn
+            v-on:click="step = 1"
+            color="grey-6"
+            label="Create another"
+            to="/"
+          />
+          <q-btn
+            v-on:click="step = 1"
+            color="grey-6"
+            label="Back to form"
+            to="/"
+          />
+        </q-btn-group>
       </q-card-actions>
     </q-card>
   </div>
--- src/pages/Start.vue
+++ src/pages/Start.vue
@@ -1,17 +1,24 @@
 <template>
-    <div class="window-height window-width row justify-center items-center">
+  <div class="window-height window-width row justify-center items-center">
     <q-card>
       <q-card-title><h1>Start Page</h1></q-card-title>
       <q-card-main>
-          <p v-for="n in 10" :key="n">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit nihil praesentium molestias a adipisci, dolore vitae odit, quidem consequatur optio voluptates asperiores pariatur eos numquam rerum delectus commodi perferendis voluptate?
-          </p>
+        <p
+          v-for="n in 10"
+          :key="n"
+        >
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit nihil praesentium molestias a adipisci, dolore vitae odit, quidem consequatur optio voluptates asperiores pariatur eos numquam rerum delectus commodi perferendis voluptate?
+        </p>
       </q-card-main>
 
-      <q-card-separator/>
+      <q-card-separator />
 
       <q-card-actions align="right">
-        <q-btn color="grey-6" label="Continue" to="/1"/>
+        <q-btn
+          color="grey-6"
+          label="Continue"
+          to="/1"
+        />
       </q-card-actions>
     </q-card>
   </div>

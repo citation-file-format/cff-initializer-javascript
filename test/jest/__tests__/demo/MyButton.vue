2a224ab3751c370ff6fbd8ab5e31cc2bf9fe121e
Faruk D <f.diblen@esciencecenter.nl>
2021-08-06 11:03:14 +0200

    add jest configuration

---
+++ test/jest/__tests__/demo/MyButton.vue
@@ -0,0 +1,14 @@
+<script lang="ts" src="./MyButton.ts"></script>
+
+<template>
+  <div>
+    <p class="content">
+      {{ input }}
+    </p>
+    <span>{{ counter }}</span>
+    <q-btn
+      class="button"
+      v-on:click="increment()"
+    />
+  </div>
+</template>

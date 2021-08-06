86587e00fbf94bc1b34cf8dcd521e9140838b00d
Stefan Verhoeven <stefan.verhoeven@gmail.com>
2021-08-06 11:51:59 +0200

    Use quasar test alpha syntax for test

--- src/pages/Start.jest.spec.ts
+++ src/pages/Start.jest.spec.ts
@@ -1,12 +1,20 @@
-import { mountFactory } from '@quasar/quasar-app-extension-testing-unit-jest';
+import { beforeEach, describe, expect, it } from '@jest/globals'
+import { installQuasarPlugin } from '@quasar/quasar-app-extension-testing-unit-jest'
+import { shallowMount, VueWrapper } from '@vue/test-utils'
 
 import Start from './Start.vue'
 
-const factory = mountFactory(Start);
+// Specify here Quasar config you'll need to test your component
+installQuasarPlugin()
 
 describe('Start', () => {
+  let wrapper: VueWrapper<any>
+
+  beforeEach(() => {
+    wrapper = shallowMount(Start)
+  })
+
   it('should mount without errors', () => {
-    const wrapper = factory();
-    expect(wrapper).toBeTruthy();
+    expect(wrapper).toBeTruthy()
   })
 })

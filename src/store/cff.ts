4663739a54ad54db863b77aaa9ff8b9d5e6d5842
Faruk D <f.diblen@esciencecenter.nl>
2021-08-10 19:25:22 +0200

    use js-yaml to generate yaml object

--- src/store/cff.ts
+++ src/store/cff.ts
@@ -1,11 +1,17 @@
 import { ref, computed } from 'vue'
+import yaml from 'js-yaml'
 
+type CFFType = 'software' | 'dataset'
 interface CFF {
+  'cff-version': string,
   title?: string,
-  message?: string
+  message?: string,
+  type: CFFType
 }
 
 const cff = ref<CFF>({
+    'cff-version': '1.2.0',
+    type: 'software'
 })
 
 export function useCFF () {
@@ -13,8 +19,24 @@ export function useCFF () {
         cff: computed(() => cff.value),
         title: computed(() => cff.value.title),
         message: computed(() => cff.value.message),
+        type: computed(() => cff.value.type),
         setTitle: (newTitle: string) => { cff.value.title = newTitle },
         setMessage: (newMessage: string) => { cff.value.message = newMessage },
-        asYAML: computed(() => JSON.stringify(cff.value, null))
+        setType: (newType: CFFType) => { cff.value.type = newType },
+        asYAML: computed(() => {
+            return yaml.dump(cff.value, {
+                replacer: hideEmptyKeys
+            })
+        })
     }
+}
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const hideEmptyKeys = (key: string, value: any) => {
+    console.log('hideEmptyKeys: ', key, value)
+    if (value === '') {
+        return undefined
+    }
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+    return value
 }

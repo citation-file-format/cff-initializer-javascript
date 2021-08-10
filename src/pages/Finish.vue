ecc37eec3040660d38156adbb94c6ab36d713700
Faruk D <f.diblen@esciencecenter.nl>
2021-08-10 13:47:02 +0200

    update layout of Finish page

--- src/pages/Finish.vue
+++ src/pages/Finish.vue
@@ -1,35 +1,150 @@
 <template>
-  <div class="window-height window-width row justify-center items-center">
-    <q-card>
-      <q-card-title><h1>Finish Page</h1></q-card-title>
-      <q-card-main>
-        <p
-          v-for="n in 10"
-          :key="n"
+  <div class="">
+    <div class="row">
+      <div class="col-6 q-pa-lg ">
+        <q-card
+          flat
+          class="secondary"
         >
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit nihil praesentium molestias a adipisci, dolore vitae odit, quidem consequatur optio voluptates asperiores pariatur eos numquam rerum delectus commodi perferendis voluptate?
-        </p>
-      </q-card-main>
+          <q-card-section>
+            <div class="text-h3">
+              <q-icon
+                class="text-h1"
+                name="check"
+              />
+              Congratulations
+            </div>
+            <!-- <div class="text-subtitle2">by John Doe</div> -->
+          </q-card-section>
 
-      <q-card-separator />
+          <q-item>
+            <q-item-section avatar>
+              <q-skeleton type="QAvatar" />
+            </q-item-section>
 
-      <q-card-actions align="center">
-        <q-btn-group flat>
-          <q-btn
-            v-on:click="step = 1"
-            color="grey-6"
-            label="Create another"
-            to="/"
+            <q-item-section>
+              <q-item-label>
+                <q-skeleton type="text" />
+              </q-item-label>
+              <q-item-label caption>
+                <q-skeleton type="text" />
+              </q-item-label>
+            </q-item-section>
+          </q-item>
+
+          <q-skeleton
+            height="200px"
+            square
           />
-          <q-btn
-            v-on:click="step = 1"
-            color="grey-6"
-            label="Back to form"
-            to="/"
+
+          <q-card-section>
+            <q-skeleton
+              type="text"
+              class="text-subtitle1"
+            />
+            <q-skeleton
+              type="text"
+              width="50%"
+              class="text-subtitle1"
+            />
+            <q-skeleton
+              type="text"
+              class="text-caption"
+            />
+          </q-card-section>
+
+          <div
+            class="text-primary q-gutter-md"
+            align="center"
+            style="font-size: 2em"
+          >
+            <q-icon
+              name="ion-logo-twitter"
+              size="xl"
+            />
+            <q-icon
+              name="ion-logo-linkedin"
+              size="xl"
+            />
+            <q-icon
+              name="ion-logo-reddit"
+              size="xl"
+            />
+          </div>
+
+          <q-card-actions
+            align="center"
+            class="q-gutter-md"
+          >
+            <q-skeleton type="QBtn" />
+            <q-skeleton type="QBtn" />
+          </q-card-actions>
+        </q-card>
+      </div>
+
+      <div class="col-6  q-pa-lg">
+        <q-card
+          flat
+          class="secondary q-mt-xl q-pt-xl"
+        >
+          <q-item>
+            <q-item-section avatar>
+              <q-skeleton type="QAvatar" />
+            </q-item-section>
+
+            <q-item-section>
+              <q-item-label>
+                <q-skeleton type="text" />
+              </q-item-label>
+              <q-item-label caption>
+                <q-skeleton type="text" />
+              </q-item-label>
+            </q-item-section>
+          </q-item>
+
+          <q-skeleton
+            height="200px"
+            square
           />
-        </q-btn-group>
-      </q-card-actions>
-    </q-card>
+
+          <q-card-section>
+            <q-skeleton
+              type="text"
+              class="text-subtitle1"
+            />
+            <q-skeleton
+              type="text"
+              width="50%"
+              class="text-subtitle1"
+            />
+            <q-skeleton
+              type="text"
+              class="text-caption"
+            />
+          </q-card-section>
+
+          <q-card-actions
+            vertical
+            align="center"
+            class="q-gutter-md"
+            size="xl"
+          >
+            <q-skeleton type="QBtn" />
+          </q-card-actions>
+        </q-card>
+      </div>
+    </div>
+
+    <div
+      class="row justify-center items-center q-pt-xl"
+    >
+      <q-btn
+        size="xl"
+        color="primary"
+        label="Finish"
+        to="/1"
+      />
+    </div>
   </div>
 </template>
 
@@ -44,3 +159,7 @@ export default defineComponent({
     }
 })
 </script>
+
+<style scoped>
+
+</style>

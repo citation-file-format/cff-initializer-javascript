f02f1f3eb8b1f505730280349608f05ba41ca182
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2021-08-11 17:04:22 +0200

    added html template indent rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -74,7 +74,13 @@ module.exports = {
     'vue/v-on-style': ['error', 'longform'],
     'vue/v-bind-style': ['error', 'longform'],
     'indent': ['error', 4],
-
+    'vue/html-indent': ['error', 4, {
+      'attribute': 1,
+      'baseIndent': 1,
+      'closeBracket': 0,
+      'alignAttributesVertically': true,
+      'ignores': []
+    }],
     // allow async-await
     'generator-star-spacing': 'off',
     // allow paren-less arrow functions

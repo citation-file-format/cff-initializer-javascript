0b681177fff2b5fa2e364a1598f4b0c886393422
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2021-08-15 17:30:15 +0200

    store now uses camelCase only; removed asYAML and cff getters from cff store

--- src/store/cff.ts
+++ src/store/cff.ts
@@ -1,41 +1,51 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 import { ref, computed } from 'vue'
-import yaml from 'js-yaml'
 import { CffType, IdentifierType, KeywordsType, TypeType } from '../types'
 
 const cff = ref<CffType>({
-    'cff-version': '1.2.0',
-    type: 'software'
+    abstract: '',
+    cffVersion: '1.2.0',
+    commit: '',
+    dateReleased: '',
+    identifiers: [],
+    keywords: [],
+    license: '',
+    message: '',
+    repository: '',
+    repositoryArtifact: '',
+    repositoryCode: '',
+    title: '',
+    type: 'software',
+    url: '',
+    version: ''
 })
 
 export function useCff () {
     return {
         abstract: computed(() => cff.value.abstract),
-        asYAML: computed(() => yaml.dump(cff.value)),
-        cff: computed(() => cff.value),
         commit: computed(() => cff.value.commit),
-        date_released: computed(() => cff.value.date_released),
+        cffVersion: computed(() => cff.value.cffVersion),
+        dateReleased: computed(() => cff.value.dateReleased),
         identifiers: computed(() => cff.value.identifiers),
         keywords: computed(() => cff.value.keywords),
         license: computed(() => cff.value.license),
         message: computed(() => cff.value.message),
         repository: computed(() => cff.value.repository),
-        repository_artifact: computed(() => cff.value.repository_artifact),
-        repository_code: computed(() => cff.value.repository_code),
+        repositoryArtifact: computed(() => cff.value.repositoryArtifact),
+        repositoryCode: computed(() => cff.value.repositoryCode),
         title: computed(() => cff.value.title),
         type: computed(() => cff.value.type),
         url: computed(() => cff.value.url),
         version: computed(() => cff.value.version),
         setAbstract: (newAbstract: string) => { cff.value.abstract = newAbstract },
         setCommit: (newCommit: string) => { cff.value.commit = newCommit },
-        setDateReleased: (newDateReleased: string) => { cff.value.date_released = newDateReleased },
+        setDateReleased: (newDateReleased: string) => { cff.value.dateReleased = newDateReleased },
         setIdentifiers: (newIdentifiers: Array<IdentifierType>) => { cff.value.identifiers = newIdentifiers },
         setKeywords: (newKeywords: KeywordsType) => { cff.value.keywords = newKeywords },
         setLicense: (newLicense: string) => { cff.value.license = newLicense },
         setMessage: (newMessage: string) => { cff.value.message = newMessage },
         setRepository: (newRepository: string) => { cff.value.repository = newRepository },
-        setRepositoryArtifact: (newRepositoryArtifact: string) => { cff.value.repository_artifact = newRepositoryArtifact },
-        setRepositoryCode: (newRepositoryCode: string) => { cff.value.repository_code = newRepositoryCode },
+        setRepositoryArtifact: (newRepositoryArtifact: string) => { cff.value.repositoryArtifact = newRepositoryArtifact },
+        setRepositoryCode: (newRepositoryCode: string) => { cff.value.repositoryCode = newRepositoryCode },
         setTitle: (newTitle: string) => { cff.value.title = newTitle },
         setType: (newType: TypeType) => { cff.value.type = newType },
         setUrl: (newUrl: string) => { cff.value.url = newUrl },

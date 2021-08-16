3f28edeac4cce6d69d2fc83811b2ba6d5d08d8b8
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2021-08-16 12:27:29 +0200

    kebabcaseKeys library now working

--- src/components/Preview.vue
+++ src/components/Preview.vue
@@ -10,9 +10,13 @@
 </template>
 
 <script lang="ts">
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import { defineComponent, computed } from 'vue'
 import { useCff } from 'src/store/cff'
 import yaml from 'js-yaml'
+import { CffType } from 'src/types'
+import kebabcaseKeys from 'kebabcase-keys'
 
 export default defineComponent({
     name: 'Preview',
@@ -35,24 +39,25 @@ export default defineComponent({
             version
         } = useCff()
 
+        const cff = kebabcaseKeys({
+            abstract: abstract.value,
+            cffVersion: cffVersion.value,
+            commit: commit.value,
+            dateReleased: dateReleased.value,
+            identifiers: identifiers.value,
+            keywords: keywords.value,
+            license: license.value,
+            message: message.value,
+            repository: repository.value,
+            repositoryArtifact: repositoryArtifact.value,
+            repositoryCode: repositoryCode.value,
+            title: title.value,
+            type: type.value,
+            url: url.value,
+            version: version.value
+        } as CffType)
         return {
-            cffstr: computed(() => yaml.dump({
-                abstract: abstract.value,
-                cffVersion: cffVersion.value,
-                commit: commit.value,
-                dateReleased: dateReleased.value,
-                identifiers: identifiers.value,
-                keywords: keywords.value,
-                license: license.value,
-                message: message.value,
-                repository: repository.value,
-                repositoryArtifact: repositoryArtifact.value,
-                repositoryCode: repositoryCode.value,
-                title: title.value,
-                type: type.value,
-                url: url.value,
-                version: version.value
-            }))
+            cffstr: computed(() => yaml.dump(cff))
         }
     }
 })
---
+++ src/kebabcase-keys.d.ts
@@ -0,0 +1,5 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+declare module 'kebabcase-keys' {
+    const kebabcaseKeys: any
+    export default kebabcaseKeys
+}

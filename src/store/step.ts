023efde61acfb697eaa77efdec9b60266f7ddb40
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2021-08-17 16:55:39 +0200

    step limits now 1-9

--- src/store/step.ts
+++ src/store/step.ts
@@ -5,8 +5,8 @@ const step = ref(1)
 export function useStep () {
     return {
         step: computed(() => step.value),
-        next: () => { step.value = step.value + 1 },
-        previous: () => { step.value = step.value - 1 },
+        next: () => { step.value = step.value < 9 ? step.value + 1 : step.value },
+        previous: () => { step.value = step.value > 1 ? step.value - 1 : step.value },
         goto: (newStep: number) => { step.value = newStep }
     }
 }

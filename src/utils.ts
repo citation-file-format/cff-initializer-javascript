14d716e612697c0b187711969020d2c493d9ddc9
Stefan Verhoeven <stefan.verhoeven@gmail.com>
2021-08-17 09:17:01 +0200

    Remove dead code

--- src/utils.ts
+++
@@ -1,9 +0,0 @@
-import yaml from 'js-yaml'
-import { CffType } from './types'
-
-export function toYamlString (obj: CffType) {
-    const j: CffType = obj
-    // TODO camelCase to kebab
-    // TODO omit empty strings
-    return yaml.dump(j)
-}

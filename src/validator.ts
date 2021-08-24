c5173af49e828d1f5ab727057895bd071e463657
Faruk D <f.diblen@esciencecenter.nl>
2021-08-24 14:37:41 +0200

    add file validation

--- src/validator.ts
+++ src/validator.ts
@@ -1,10 +1,17 @@
 import Ajv from 'ajv'
 import addFormats from 'ajv-formats'
 import schema from './schemas/1.2.0/schema.json'
+import { useCffstr } from './store/cffstr'
 
 export const ajv = new Ajv()
 addFormats(ajv)
 ajv.addSchema(schema)
+
+export const isValidCffFile = () => {
+    const { jsObject } = useCffstr()
+    const isValid = ajv.validate(`${schema.$id}`, jsObject.value)
+    return isValid
+}
 
 export const makeFieldValidator = (subschema: string) => {
     return (val: unknown) => {

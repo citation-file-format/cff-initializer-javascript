ad47593c2e0e8935aeadd7d4c6f592014c95eeaa
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2021-10-18 12:27:20 +0200

    refactored methods to use fat arrow syntax for consistency; sorted methods

--- src/components/ScreenIdentifiers.vue
+++ src/components/ScreenIdentifiers.vue
@@ -73,47 +73,52 @@ export default defineComponent({
     setup () {
         const { identifiers, setIdentifiers } = useCff()
         const editingId = ref(-1)
-        return {
-            identifiers,
-            editingId,
-            setIdentifierTypeField (field: keyof IdentifierType, value: IdentifierTypeType) {
-                const identifier = { ...identifiers.value[editingId.value] }
-                identifier.type = value
-                identifiers.value[editingId.value] = identifier
-                setIdentifiers(identifiers.value)
-            },
-            setIdentifierValueField (field: keyof IdentifierType, value: string) {
-                const identifier = { ...identifiers.value[editingId.value] }
-                identifier.value = value
-                identifiers.value[editingId.value] = identifier
-                setIdentifiers(identifiers.value)
-            },
-            setIdentifierDescriptionField (field: keyof IdentifierType, value: string) {
-                const identifier = { ...identifiers.value[editingId.value] }
-                identifier.description = value
-                identifiers.value[editingId.value] = identifier
-                setIdentifiers(identifiers.value)
-            },
-            removeIdentifier () {
-                const newIdentifiers = [...identifiers.value]
-                newIdentifiers.splice(editingId.value, 1)
-                setIdentifiers(newIdentifiers)
-                editingId.value = -1
-            },
-            addIdentifier () {
-                const newIdentifier: IdentifierType = {
-                    type: 'doi',
-                    value: '',
-                    description: ''
-                }
-                const newIdentifiers = [...identifiers.value, newIdentifier]
-                setIdentifiers(newIdentifiers)
-                editingId.value = newIdentifiers.length - 1
-                setTimeout(() => {
-                    // FIXME shouldn't have to use a timeout but it seems the DOM doesn't update in time
-                    document.getElementsByClassName('bottom')[0].scrollIntoView({ behavior: 'smooth' })
-                }, 100)
+        const addIdentifier = () => {
+            const newIdentifier: IdentifierType = {
+                type: 'doi',
+                value: '',
+                description: ''
             }
+            const newIdentifiers = [...identifiers.value, newIdentifier]
+            setIdentifiers(newIdentifiers)
+            editingId.value = newIdentifiers.length - 1
+            setTimeout(() => {
+                // FIXME shouldn't have to use a timeout but it seems the DOM doesn't update in time
+                document.getElementsByClassName('bottom')[0].scrollIntoView({ behavior: 'smooth' })
+            }, 100)
+        }
+        const removeIdentifier = () => {
+            const newIdentifiers = [...identifiers.value]
+            newIdentifiers.splice(editingId.value, 1)
+            setIdentifiers(newIdentifiers)
+            editingId.value = -1
+        }
+        const setIdentifierDescriptionField = (field: keyof IdentifierType, value: string) => {
+            const identifier = { ...identifiers.value[editingId.value] }
+            identifier.description = value
+            identifiers.value[editingId.value] = identifier
+            setIdentifiers(identifiers.value)
+        }
+        const setIdentifierTypeField = (field: keyof IdentifierType, value: IdentifierTypeType) => {
+            const identifier = { ...identifiers.value[editingId.value] }
+            identifier.type = value
+            identifiers.value[editingId.value] = identifier
+            setIdentifiers(identifiers.value)
+        }
+        const setIdentifierValueField = (field: keyof IdentifierType, value: string) => {
+            const identifier = { ...identifiers.value[editingId.value] }
+            identifier.value = value
+            identifiers.value[editingId.value] = identifier
+            setIdentifiers(identifiers.value)
+        }
+        return {
+            addIdentifier,
+            editingId,
+            identifiers,
+            removeIdentifier,
+            setIdentifierDescriptionField,
+            setIdentifierTypeField,
+            setIdentifierValueField
         }
     }
 })

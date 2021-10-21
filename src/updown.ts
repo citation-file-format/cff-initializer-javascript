42cb7673b265d7cdf0083b238bcbf5e3c4d62619
Abel Soares Siqueira <abel.s.siqueira@gmail.com>
2021-10-21 17:49:43 +0200

    More refactoring

--- src/updown.ts
+++ src/updown.ts
@@ -1,13 +1,15 @@
+function moveUpdate<Type> (index: number, value: number, array: Type[], updateArray: (a: Type[]) => void): void {
+    const newArray: Type[] = [...array]
+    newArray[index] = newArray.splice(index + value, 1, newArray[index])[0]
+    updateArray(newArray)
+}
+
 export function moveDown<Type> (index: number, array: Type[], updateArray: (a: Type[]) => void): void {
     if (index === array.length - 1) return
-    const newArray: Type[] = [...array]
-    newArray[index] = newArray.splice(index + 1, 1, newArray[index])[0]
-    updateArray(newArray)
+    moveUpdate(index, 1, array, updateArray)
 }
 
 export function moveUp<Type> (index: number, array: Type[], updateArray: (a: Type[]) => void): void {
     if (index === 0) return
-    const newArray: Type[] = [...array]
-    newArray[index] = newArray.splice(index - 1, 1, newArray[index])[0]
-    updateArray(newArray)
+    moveUpdate(index, -1, array, updateArray)
 }

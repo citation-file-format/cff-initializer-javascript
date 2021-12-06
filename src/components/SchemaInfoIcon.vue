a94bb3a9fa040dd31d5df78b6b1f19e2e98f272b
Abel Soares Siqueira <abel.s.siqueira@gmail.com>
2021-12-06 17:23:56 +0100

    Create SchemaInfoIcon with an icon that points to the schema guide

---
+++ src/components/SchemaInfoIcon.vue
@@ -0,0 +1,25 @@
+<template>
+    <a v-bind:href="'https://github.com/citation-file-format/citation-file-format/blob/main/schema-guide.md#' + section">
+        <q-icon name="ion-information-circle-outline" />
+    </a>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    name: 'SchemaInfoIcon',
+    props: {
+        section: {
+            type: String,
+            required: true
+        }
+    }
+})
+</script>
+
+<style scoped>
+a {
+    text-decoration: none;
+}
+</style>

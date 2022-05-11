b1bd484f1bc139b004326fc48a76e6ad9e995761
Abel Soares Siqueira <abel.s.siqueira@gmail.com>
2022-05-11 14:16:30 +0200

    Add tooltip to info icons: 'Click to access specific documentation'

--- src/components/SchemaGuideLink.vue
+++ src/components/SchemaGuideLink.vue
@@ -1,6 +1,13 @@
 <template>
     <a v-bind:href="'https://github.com/citation-file-format/citation-file-format/blob/1.2.0/schema-guide.md' + anchor">
         <q-icon name="ion-information-circle-outline" />
+        <q-tooltip
+            anchor="center middle"
+            class="bg-primary text-body2 text-white"
+            self="top left"
+        >
+            Click to access specific documentation
+        </q-tooltip>
     </a>
 </template>
 

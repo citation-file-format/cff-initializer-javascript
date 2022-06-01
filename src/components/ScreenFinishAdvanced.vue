127ee729d48fa35465d04487ad8149525c9e83ed
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2022-06-01 21:37:48 +0200

    added reset method for resetting the stepper state onReset'ing the form

--- src/components/ScreenFinishAdvanced.vue
+++ src/components/ScreenFinishAdvanced.vue
@@ -57,6 +57,7 @@ import Stepper from 'components/Stepper.vue'
 import StepperActions from 'components/StepperActions.vue'
 import { useValidation } from 'src/store/validation'
 import DownloadButton from 'components/DownloadButton.vue'
+import { useStepperErrors } from 'src/store/stepper-errors'
 
 export default defineComponent({
     name: 'ScreenFinishAdvanced',
@@ -67,13 +68,15 @@ export default defineComponent({
     },
     setup () {
         const { setStepName, setShowAdvanced } = useApp()
-        const { reset } = useCff()
+        const { reset: resetCffData } = useCff()
+        const { reset: resetStepperErrorState } = useStepperErrors()
         const { errors } = useValidation()
-
+        console.log(errors.value)
         return {
             isValidCFF: computed(() => errors.value.length === 0),
             createAnother: async () => {
-                reset()
+                resetCffData()
+                resetStepperErrorState()
                 setShowAdvanced(false)
                 await setStepName('start')
             },
--- src/store/stepper-errors.ts
+++ src/store/stepper-errors.ts
@@ -1,13 +1,17 @@
 import { computed, ref } from 'vue'
 
-const state = ref({
-    authors: true,
-    identifiers: false,
-    keywords: false,
-    relatedResources: false,
-    start: true,
-    versionSpecific: false
-})
+const reset = () => {
+    return {
+        authors: true,
+        identifiers: false,
+        keywords: false,
+        relatedResources: false,
+        start: true,
+        versionSpecific: false
+    }
+}
+
+const state = ref(reset())
 
 export const useStepperErrors = () => {
     return {
@@ -17,6 +21,7 @@ export const useStepperErrors = () => {
         errorStateScreenRelatedResources: computed(() => state.value.relatedResources),
         errorStateScreenStart: computed(() => state.value.start),
         errorStateScreenVersionSpecific: computed(() => state.value.versionSpecific),
+        reset: () => (state.value = reset()),
         setErrorStateScreenAuthors: (hasErrors: boolean) => (state.value.authors = hasErrors),
         setErrorStateScreenIdentifiers: (hasErrors: boolean) => (state.value.identifiers = hasErrors),
         setErrorStateScreenKeywords: (hasErrors: boolean) => (state.value.keywords = hasErrors),

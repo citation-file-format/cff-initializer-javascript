a719cb26f47be518aef4e646354938ca3a5dc52b
Faruk D <f.diblen@esciencecenter.nl>
2022-06-02 12:01:00 +0200

    do not open web browser automatically

--- quasar.conf.js
+++ quasar.conf.js
@@ -82,7 +82,7 @@ module.exports = configure(function () {
         devServer: {
             https: false,
             port: 8080,
-            open: true // opens browser window automatically
+            open: false // do not open browser window automatically
         },
 
         // https://v2.quasar.dev/quasar-cli/quasar-conf-js#Property%3A-framework

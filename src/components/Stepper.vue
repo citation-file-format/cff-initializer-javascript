151bbd97eea6bc3d6a7fd17da1b92f7a956cf5aa
Abel Soares Siqueira <abel.s.siqueira@gmail.com>
2022-06-08 15:36:23 +0200

    Move properties of stepper elements around to improve readability

--- src/components/Stepper.vue
+++ src/components/Stepper.vue
@@ -1,35 +1,30 @@
 <template>
     <q-stepper
-        active-icon=""
         animated
+        color="primary"
         class="bg-secondary text-primary"
+        error-color="negative"
+        error-icon="warning"
         flat
         header-nav
+        inactive-color="primary"
         inactive-icon=""
         v-bind:model-value="stepName"
         vertical
     >
         <q-step
-            error-color="negative"
-            error-icon="warning"
-            icon=""
             name="start"
             title="Start"
             v-bind:active-icon="errorStateScreenStart ? 'warning' : 'edit'"
-            v-bind:color="errorStateScreenStart ? 'negative' : 'primary'"
             v-bind:error="errorStateScreenStart"
             v-bind:order="0"
             v-on:click="setStepName('start')"
         />
 
         <q-step
-            error-color="negative"
-            error-icon="warning"
-            icon=""
             name="authors"
             title="Authors"
             v-bind:active-icon="errorStateScreenAuthors ? 'warning' : 'edit'"
-            v-bind:color="errorStateScreenAuthors ? 'negative' : 'primary'"
             v-bind:error="errorStateScreenAuthors"
             v-bind:order="1"
             v-on:click="setStepName('authors')"
@@ -37,8 +32,6 @@
 
         <q-step
             active-icon="navigate_next"
-            color="primary"
-            icon=""
             name="finish-minimum"
             title="Finish"
             v-bind:order="2"
@@ -47,13 +40,9 @@
         />
 
         <q-step
-            error-color="negative"
-            error-icon="warning"
-            icon=""
             name="identifiers"
             title="Identifiers"
             v-bind:active-icon="errorStateScreenIdentifiers ? 'warning' : 'edit'"
-            v-bind:color="errorStateScreenIdentifiers ? 'negative' : 'primary'"
             v-bind:error="errorStateScreenIdentifiers"
             v-bind:order="3"
             v-if="showAdvanced"
@@ -61,21 +50,16 @@
         />
 
         <q-step
-            error-color="negative"
-            error-icon="warning"
-            icon=""
             name="related-resources"
             title="Related resources"
             v-bind:active-icon="errorStateScreenRelatedResources ? 'warning' : 'edit'"
-            v-bind:color="errorStateScreenRelatedResources ? 'negative' : 'primary'"
             v-bind:error="errorStateScreenRelatedResources"
             v-bind:order="4"
             v-if="showAdvanced"
             v-on:click="setStepName('related-resources')"
         />
+
         <q-step
-            color="primary"
-            icon=""
             name="abstract"
             title="Abstract"
             v-bind:order="5"
@@ -84,13 +68,9 @@
         />
 
         <q-step
-            error-color="negative"
-            error-icon="warning"
-            icon=""
             name="keywords"
             title="Keywords"
             v-bind:active-icon="errorStateScreenKeywords ? 'warning' : 'edit'"
-            v-bind:color="errorStateScreenKeywords ? 'negative' : 'primary'"
             v-bind:error="errorStateScreenKeywords"
             v-bind:order="6"
             v-if="showAdvanced"
@@ -98,8 +78,6 @@
         />
 
         <q-step
-            color="primary"
-            icon=""
             name="license"
             title="License"
             v-bind:order="7"
@@ -108,22 +86,17 @@
         />
 
         <q-step
-            error-color="negative"
-            error-icon="warning"
-            icon=""
             name="version-specific"
             title="Version specific"
             v-bind:active-icon="errorStateScreenVersionSpecific ? 'warning' : 'edit'"
-            v-bind:color="errorStateScreenVersionSpecific ? 'negative' : 'primary'"
             v-bind:error="errorStateScreenVersionSpecific"
             v-bind:order="8"
             v-if="showAdvanced"
             v-on:click="setStepName('version-specific')"
         />
+
         <q-step
             active-icon="navigate_next"
-            color="primary"
-            icon=""
             name="finish-advanced"
             title="Finish"
             v-bind:order="9"

aa24432c901ea1fac146a60475ce757c064c0949
Faruk D <f.diblen@esciencecenter.nl>
2022-08-25 10:42:26 +0200

    move lastname i icons outside

--- src/components/AuthorCardEditing.vue
+++ src/components/AuthorCardEditing.vue
@@ -1,9 +1,5 @@
 <template>
-    <q-card
-        flat
-        bordered
-        class="bg-formcard q-pa-md"
-    >
+    <q-card flat bordered class="bg-formcard q-pa-md">
         <div class="row">
             <h3 class="subquestion">
                 The person's given names
@@ -11,81 +7,37 @@
             </h3>
         </div>
         <div class="row">
-            <q-input
-                autofocus
-                bg-color="white"
-                class="col"
-                dense
-                label="given-names"
-                outlined
-                standout
-                title="The person's given names."
-                v-bind:model-value="givenNames"
-                v-bind:error="false"
-                v-bind:error-message="''"
-                v-on:update:modelValue="$emit('update', 'givenNames', $event)"
-            />
+            <q-input autofocus bg-color="white" class="col" dense label="given-names" outlined standout
+                title="The person's given names." v-bind:model-value="givenNames" v-bind:error="false"
+                v-bind:error-message="''" v-on:update:modelValue="$emit('update', 'givenNames', $event)" />
         </div>
         <div class="row">
             <h3 class="subquestion">
                 The person's last names, split into parts
+                <q-icon
+                    name="ion-information-circle-outline"
+                    size="24px"
+                    color="primary"
+                    v-on:click="showLastNameHelp = true"
+                    style="cursor:pointer;"
+                />
+                <SchemaGuideLink anchor="#definitionspersonname-particle" />
+                <SchemaGuideLink anchor="#definitionspersonname-suffix" class="q-pt-sm" />
+                <SchemaGuideLink anchor="#definitionspersonfamily-names" class="q-pt-sm" />
             </h3>
         </div>
         <div class="row">
-            <q-input
-                bg-color="white"
-                class="col-3"
-                dense
-                label="name-particle"
-                outlined
-                standout
+            <q-input bg-color="white" class="col-3" dense label="name-particle" outlined standout
                 title="The person's name particle, e.g., a nobiliary particle or a [preposition] meaning 'of' or 'from' (for example 'von' in 'Alexander von Humboldt')."
-                v-bind:model-value="nameParticle"
-                v-bind:error="false"
-                v-bind:error-message="''"
-                v-on:update:modelValue="$emit('update', 'nameParticle', $event)"
-            >
-                <SchemaGuideLink
-                    anchor="#definitionspersonname-particle"
-                    class="q-pt-sm"
-                />
-            </q-input>
-            <q-input
-                bg-color="white"
-                class="col"
-                dense
-                label="family-names"
-                outlined
-                standout
-                title="The person's family names."
-                v-bind:model-value="familyNames"
-                v-bind:error="false"
-                v-bind:error-message="''"
-                v-on:update:modelValue="$emit('update', 'familyNames', $event)"
-            >
-                <SchemaGuideLink
-                    anchor="#definitionspersonfamily-names"
-                    class="q-pt-sm"
-                />
-            </q-input>
-            <q-input
-                bg-color="white"
-                class="col-3"
-                dense
-                label="name-suffix"
-                outlined
-                standout
+                v-bind:model-value="nameParticle" v-bind:error="false" v-bind:error-message="''"
+                v-on:update:modelValue="$emit('update', 'nameParticle', $event)" />
+            <q-input bg-color="white" class="col" dense label="family-names" outlined standout
+                title="The person's family names." v-bind:model-value="familyNames" v-bind:error="false"
+                v-bind:error-message="''" v-on:update:modelValue="$emit('update', 'familyNames', $event)" />
+            <q-input bg-color="white" class="col-3" dense label="name-suffix" outlined standout
                 title="The person's name suffix, e.g. 'Jr.' for Sammy Davis Jr. or 'III' for Frank Edwin Wright III."
-                v-bind:model-value="nameSuffix"
-                v-bind:error="false"
-                v-bind:error-message="''"
-                v-on:update:modelValue="$emit('update', 'nameSuffix', $event)"
-            >
-                <SchemaGuideLink
-                    anchor="#definitionspersonname-suffix"
-                    class="q-pt-sm"
-                />
-            </q-input>
+                v-bind:model-value="nameSuffix" v-bind:error="false" v-bind:error-message="''"
+                v-on:update:modelValue="$emit('update', 'nameSuffix', $event)" />
         </div>
         <div class="row">
             <h3 class="subquestion">
@@ -94,21 +46,11 @@
             </h3>
         </div>
         <div class="row">
-            <q-input
-                bg-color="white"
-                class="col"
-                dense
-                label="email"
-                outlined
-                standout
-                title="The person's email address."
-                type="email"
-                v-bind:model-value="email"
-                v-bind:class="emailErrors.length > 0 ? 'has-error' : ''"
-                v-bind:error="emailErrors.length > 0"
+            <q-input bg-color="white" class="col" dense label="email" outlined standout
+                title="The person's email address." type="email" v-bind:model-value="email"
+                v-bind:class="emailErrors.length > 0 ? 'has-error' : ''" v-bind:error="emailErrors.length > 0"
                 v-bind:error-message="emailErrors.join(', ')"
-                v-on:update:modelValue="$emit('update', 'email', $event)"
-            />
+                v-on:update:modelValue="$emit('update', 'email', $event)" />
         </div>
         <div class="row">
             <h3 class="subquestion col">
@@ -121,58 +63,29 @@
             </h3>
         </div>
         <div class="row">
-            <q-input
-                bg-color="white"
-                class="col"
-                dense
-                label="affiliation"
-                outlined
-                standout
-                title="The person's affiliation."
-                v-bind:model-value="affiliation"
-                v-bind:error="false"
-                v-bind:error-message="''"
-                v-on:update:modelValue="$emit('update', 'affiliation', $event)"
-            />
-            <q-input
-                bg-color="white"
-                class="col"
-                dense
-                hint="Format: https://orcid.org/0000-0000-0000-0000"
-                mask="https://orcid.org/####-####-####-###X"
-                outlined
-                standout
-                title="The person's ORCID identifier."
-                v-bind:class="orcidErrors.length > 0 ? 'has-error' : ''"
-                v-bind:model-value="orcid"
-                v-bind:error="orcidErrors.length > 0"
+            <q-input bg-color="white" class="col" dense label="affiliation" outlined standout
+                title="The person's affiliation." v-bind:model-value="affiliation" v-bind:error="false"
+                v-bind:error-message="''" v-on:update:modelValue="$emit('update', 'affiliation', $event)" />
+            <q-input bg-color="white" class="col" dense hint="Format: https://orcid.org/0000-0000-0000-0000"
+                label="orcid" mask="https://orcid.org/####-####-####-###X" outlined standout
+                title="The person's ORCID identifier." v-bind:class="orcidErrors.length > 0 ? 'has-error' : ''"
+                v-bind:model-value="orcid" v-bind:error="orcidErrors.length > 0"
                 v-bind:error-message="orcidErrors.join(', ')"
-                v-on:update:modelValue="$emit('update', 'orcid', $event)"
-            />
+                v-on:update:modelValue="$emit('update', 'orcid', $event)" />
         </div>
 
         <q-card-actions align="right">
-            <q-btn
-                color="negative"
-                dense
-                icon="delete"
-                label="Remove"
-                v-on:click="$emit('removePressed')"
-            />
-            <q-btn
-                data-cy="btn-done"
-                dense
-                icon="done"
-                label="Done"
-                v-on:click="$emit('closePressed')"
-            />
+            <q-btn color="negative" dense icon="delete" label="Remove" v-on:click="$emit('removePressed')" />
+            <q-btn dense icon="done" label="Done" v-on:click="$emit('closePressed')" />
         </q-card-actions>
     </q-card>
+    <InfoDialog v-model="showLastNameHelp" v-bind:data="helpData.lastName" />
 </template>
 
 <script lang="ts">
 import { byError, emailQueries, orcidQueries } from 'src/error-filtering'
-import { computed, defineComponent, onUpdated } from 'vue'
+import { computed, defineComponent, onUpdated, ref } from 'vue'
+import InfoDialog from 'components/InfoDialog.vue'
 import SchemaGuideLink from 'src/components/SchemaGuideLink.vue'
 import { useStepperErrors } from 'src/store/stepper-errors'
 import { useValidation } from 'src/store/validation'
@@ -229,9 +142,58 @@ export default defineComponent({
                 .filter(byError(errors.value))
                 .map(query => query.replace.message)
         })
+        const helpData = {
+            name: {
+                title: 'name',
+                url: 'https://github.com/citation-file-format/citation-file-format/blob/1.2.0/schema-guide.md#type',
+                description: 'The type of the work that is being described by this CITATION.cff file.'
+            },
+            lastName: {
+                title: 'lastName',
+                url: 'https://github.com/citation-file-format/citation-file-format/blob/1.2.0/schema-guide.md#title',
+                description: 'The name of the software or dataset.',
+                examples: [
+                    'cffconvert',
+                    'Firefox',
+                    'LibreOffice'
+                ]
+            },
+            email: {
+                title: 'email',
+                url: 'https://github.com/citation-file-format/citation-file-format/blob/1.2.0/schema-guide.md#message',
+                description: 'A message to the human reader of the CITATION.cff file to let them know what to do with the citation metadata.',
+                examples: [
+                    'If you use this software, please cite it using the metadata from this file.',
+                    'Please cite this software using these metadata.',
+                    'Please cite this software using the metadata from "preferred-citation".'
+                ]
+            },
+            affiliation: {
+                title: 'affiliation',
+                url: 'https://github.com/citation-file-format/citation-file-format/blob/1.2.0/schema-guide.md#message',
+                description: 'A message to the human reader of the CITATION.cff file to let them know what to do with the citation metadata.',
+                examples: [
+                    'If you use this software, please cite it using the metadata from this file.',
+                    'Please cite this software using these metadata.',
+                    'Please cite this software using the metadata from "preferred-citation".'
+                ]
+            },
+            orcid: {
+                title: 'orcid',
+                url: 'https://github.com/citation-file-format/citation-file-format/blob/1.2.0/schema-guide.md#message',
+                description: 'A message to the human reader of the CITATION.cff file to let them know what to do with the citation metadata.',
+                examples: [
+                    'If you use this software, please cite it using the metadata from this file.',
+                    'Please cite this software using these metadata.',
+                    'Please cite this software using the metadata from "preferred-citation".'
+                ]
+            }
+        }
         return {
             emailErrors,
-            orcidErrors
+            helpData,
+            orcidErrors,
+            showLastNameHelp: ref(false),
         }
     },
     emits: ['closePressed', 'removePressed', 'update'],

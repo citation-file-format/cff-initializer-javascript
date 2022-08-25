db42e99fcc2625916e9b434aacce689bdb5495ca
Abel Soares Siqueira <abel.s.siqueira@gmail.com>
2022-08-25 13:20:20 +0200

    Implements @fdiblen's suggestions from the PR review
    Add more information to README.dev.md
    Remove cypress/support and cypress/fixtures

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'cypress'
 
 export default defineConfig({
     e2e: {
-        baseUrl: 'http://localhost:8080/cff-initializer-javascript/#'
+        baseUrl: 'http://localhost:8080/cff-initializer-javascript/#',
+        supportFile: false // If the support/e2e.ts or support/commands.ts files are reinstated, delete this line
     }
 })

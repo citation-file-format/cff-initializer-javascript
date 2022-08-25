21bec90d78f4b6d10ece66991cc98b6aa59cfe3b
Reggie Cushing <reggie@Reggies-MacBook-Pro.local>
2022-08-25 10:54:29 +0200

    adding fill width for download and add more  button

--- src/components/DownloadButton.vue
+++ src/components/DownloadButton.vue
@@ -8,12 +8,14 @@
         type="a"
         download="CITATION.cff"
         v-bind:href="downloadUrl"
+        v-bind:class="$q.platform.is.mobile ? 'full-width' : ''"
     />
 </template>
 
 <script lang="ts">
 import { computed, defineComponent } from 'vue'
 import { useCffstr } from 'src/store/cffstr'
+import { useQuasar } from 'quasar'
 
 const toDownloadUrl = (body: string) => {
     return `data:text/vnd.yaml,${encodeURIComponent(body)}`
@@ -23,8 +25,9 @@ export default defineComponent({
     name: 'DownloadButton',
     setup () {
         const { cffstr } = useCffstr()
-
+        const $q = useQuasar()
         return {
+            $q,
             downloadUrl: computed(() => toDownloadUrl(cffstr.value))
         }
     }

6947dec1fcde81cd8d99eee042c6a69098775dfd
Reggie Cushing <reggie@Reggies-MBP.lan>
2022-08-26 14:25:44 +0200

    change $q to q to remove vue warning

--- src/components/DownloadButton.vue
+++ src/components/DownloadButton.vue
@@ -9,7 +9,7 @@
         type="a"
         download="CITATION.cff"
         v-bind:href="downloadUrl"
-        v-bind:class="$q.platform.is.mobile ? 'full-width' : ''"
+        v-bind:class="q.platform.is.mobile ? 'full-width' : ''"
     />
 </template>
 
@@ -26,9 +26,9 @@ export default defineComponent({
     name: 'DownloadButton',
     setup () {
         const { cffstr } = useCffstr()
-        const $q = useQuasar()
+        const q = useQuasar()
         return {
-            $q,
+            q,
             downloadUrl: computed(() => toDownloadUrl(cffstr.value))
         }
     }

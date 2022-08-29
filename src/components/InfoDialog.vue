5aee6bf8323fd05927a8c7b1b358730c8832fb00
Faruk D <f.diblen@esciencecenter.nl>
2022-08-29 16:30:45 +0200

    fix extra margin

--- src/components/InfoDialog.vue
+++ src/components/InfoDialog.vue
@@ -1,69 +1,65 @@
 <template>
-    <div
-        class="q-pa-md q-gutter-sm"
+    <q-dialog
+        v-model="showDialog"
     >
-        <q-dialog
-            v-model="showDialog"
+        <q-card
+            class="window-width help-dialog"
         >
-            <q-card
-                class="window-width help-dialog"
-            >
-                <q-card-section class="row items-center q-pb-none">
-                    <div class="text-h5">
-                        CFF field: {{ data.title }}
-                    </div>
-                    <q-space />
-                    <q-btn
-                        icon="close"
-                        flat
-                        round
-                        dense
-                        v-close-popup
-                    />
-                </q-card-section>
+            <q-card-section class="row items-center q-pb-none">
+                <div class="text-h5">
+                    CFF field: {{ data.title }}
+                </div>
+                <q-space />
+                <q-btn
+                    icon="close"
+                    flat
+                    round
+                    dense
+                    v-close-popup
+                />
+            </q-card-section>
 
-                <q-card-section>
-                    {{ data.description }}
-                </q-card-section>
-                <q-card-section v-if="data.examples">
-                    <div class="text-h6">
-                        Examples
-                    </div>
-                    <ul>
-                        <li
-                            v-for="item in data.examples"
-                            v-bind:key="item"
-                            class="q-mt-sm"
-                        >
-                            <div
-                                v-for="(line, index) in item.split('\n')"
-                                v-bind:key="index"
-                            >
-                                {{ line }}
-                            </div>
-                        </li>
-                    </ul>
-                </q-card-section>
-                <q-card-section v-if="data.url">
-                    <div class="text-h6">
-                        Related links
-                    </div>
-                    <div
-                        v-for="item in data.url"
+            <q-card-section>
+                {{ data.description }}
+            </q-card-section>
+            <q-card-section v-if="data.examples">
+                <div class="text-h6">
+                    Examples
+                </div>
+                <ul>
+                    <li
+                        v-for="item in data.examples"
                         v-bind:key="item"
+                        class="q-mt-sm"
                     >
-                        <a
-                            v-bind:href="item.link"
-                            tabindex="-1"
-                            target="_blank"
+                        <div
+                            v-for="(line, index) in item.split('\n')"
+                            v-bind:key="index"
                         >
-                            {{ item.text }}
-                        </a>
-                    </div>
-                </q-card-section>
-            </q-card>
-        </q-dialog>
-    </div>
+                            {{ line }}
+                        </div>
+                    </li>
+                </ul>
+            </q-card-section>
+            <q-card-section v-if="data.url">
+                <div class="text-h6">
+                    Related links
+                </div>
+                <div
+                    v-for="item in data.url"
+                    v-bind:key="item"
+                >
+                    <a
+                        v-bind:href="item.link"
+                        tabindex="-1"
+                        target="_blank"
+                    >
+                        {{ item.text }}
+                    </a>
+                </div>
+            </q-card-section>
+        </q-card>
+    </q-dialog>
 </template>
 
 <script lang="ts">

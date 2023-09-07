cb06ccf0b77e492e1a75d48dc5cb7d819bd991dc
Abel Soares Siqueira <abel.s.siqueira@gmail.com>
2023-09-07 17:06:09 +0200

    [WIP] Trying to setup Matomo

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,16 @@
 <template>
+    <PrivacyNotice />
     <router-view />
 </template>
+
 <script lang="ts">
+import PrivacyNotice from 'src/components/PrivacyNotice.vue'
 
 export default ({
-    name: 'App'
+    name: 'App',
+    components: {
+        PrivacyNotice
+    }
 })
 
 </script>
---
+++ src/components/PrivacyNotice.vue
@@ -0,0 +1,38 @@
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { useQuasar } from 'quasar'
+
+export default defineComponent({
+    name: 'PrivacyNotice',
+    setup () {
+        const $q = useQuasar()
+        const tempWindow = window as any
+
+        const _paq = tempWindow._paq = tempWindow._paq || []
+
+        const accept = () => {
+            _paq.push(['rememberConsentGiven'])
+            _paq.push(['setConsentGiven'])
+        }
+
+        const refuse = () => {
+            _paq.push(['forgetConsentGiven'])
+        }
+
+        $q.notify({
+            message: 'We use <a href="/cookies" class="link-over-primary" target="_blank">cookies</a> with <a href="https://matomo.org" class="link-over-primary" target="_black">matomo.org</a> to anonymously track usage and improve our app. Do you accept?',
+            color: 'primary',
+            progress: true,
+            timeout: 0,
+            actions: [
+                { label: 'Accept', color: 'white', handler: accept },
+                { label: 'Refuse', color: 'white', handler: refuse }
+            ],
+            html: true
+        })
+
+        return {
+        }
+    }
+})
+</script>
